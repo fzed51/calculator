01f9275a71eee07660a36a68d7d25a8bffa9b616
Fabien Sanchez <fzed51+git@gmail.com>
2024-06-16 22:22:00 +0200

    fix import

--- src/components/datetime-calculator/index.tsx
+++ src/components/datetime-calculator/index.tsx
@@ -1,10 +1,10 @@
 import { useMemo, useState } from "react";
 import { Operator } from "./types";
 import { Stack } from "@mui/material";
-import StartDatetimeForm from "./start-datetime-form";
-import OperatorForm from "./operator-form";
-import DiffTimeForm from "./diff-time-form";
-import ResultDisplay from "./result-display";
+import { StartDatetimeForm } from "./start-datetime-form";
+import { OperatorForm } from "./operator-form";
+import { DiffTimeForm } from "./diff-time-form";
+import { ResultDisplay } from "./result-display";
 
 export function DatetimeCalculator() {
     const [startDatetime, setStartDatetime] = useState<Date>(new Date());
@@ -22,6 +22,6 @@ export function DatetimeCalculator() {
         <StartDatetimeForm />
         <OperatorForm />
         <DiffTimeForm />
-        <ResultDisplay result={result}/>
+        <ResultDisplay result={result} />
     </Stack>
 }

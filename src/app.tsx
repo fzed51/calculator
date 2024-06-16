ba9bb73e989af8a417fdd9328258612d0e4c841a
Fabien Sanchez <fzed51+git@gmail.com>
2024-06-16 23:11:38 +0200

    update StartDatetimeForm

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,19 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./pages/router";
 
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import dayjs from 'dayjs'
+import 'dayjs/locale/fr'
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { theme } from "./theme";
+dayjs.locale('fr')
 
 export function App() {
-  return <RouterProvider router={router} />
+  return <LocalizationProvider dateAdapter={AdapterDayjs}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </LocalizationProvider>
 };
--- src/components/datetime-calculator/start-datetime-form.tsx
+++ src/components/datetime-calculator/start-datetime-form.tsx
@@ -1,9 +1,27 @@
-import React, { FC } from "react";
+import { Box, FormControl, FormControlLabel, Radio, RadioGroup } from "@mui/material";
+import { ChangeEvent, useState } from "react";
 
-export interface StartDatetimeFormProps { }
+export interface StartDatetimeFormProps {
+    value: Date;
+    onChange: (d: Date) => void
+}
 
-export const StartDatetimeForm: FC<StartDatetimeFormProps> = ({ }) => {
-    return <>StartDatetimeForm</>;
+export function StartDatetimeForm({ }: StartDatetimeFormProps) {
+    const [dateSelector, setDateSelector] = useState<"now" | "manuel">("now");
+
+    const handleSelect = (_: ChangeEvent<HTMLInputElement>, value: string) => {
+        setDateSelector(value as "now" | "manuel")
+    }
+
+    return <Box alignItems="center">
+        <FormControl>
+            <RadioGroup
+                value={dateSelector}
+                onChange={handleSelect}
+            >
+                <FormControlLabel value="female" control={<Radio />} label="Maintenant" />
+                <FormControlLabel value="male" control={<Radio />} label="Manuel" />
+            </RadioGroup>
+        </FormControl>
+    </Box>;
 };
-
-export default StartDatetimeForm;
